c212bf4bb9b5c0b106012c3d3dd1601627347b2d
DORI <dbswhdrbs@naver.com>
2015-12-15 01:31:47 +0900

    Simple Web Server Study

---
+++ SimpleWebServer/DBSQL/SqlEx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWebServer.DBSQL
+{
+    public class SqlEx
+    {
+        public delegate void QueryExecCallback(SqlDataReader dataReader);
+
+        /// <summary>
+        /// DB 접속을 위한 접속 정보
+        /// </summary>
+        private static string _connectionString = string.Empty;
+
+        private static string connectionString
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_connectionString))
+                {
+                    _connectionString = string.Format("Data Source={0};Initial Catalog={1};user id={2};Password={3}",
+                        Properties.Settings.Default.DATA_SOURCE,
+                        Properties.Settings.Default.CATALOG,
+                        Properties.Settings.Default.ID,
+                        Properties.Settings.Default.PASSWORD);
+                }
+
+                return _connectionString;
+            }
+        }
+
+        public void ExecuteQuery(QueryExecCallback queryExecCallback, string queryText, CommandType type = CommandType.Text)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = connection.CreateCommand())
+                    {
+                        command.CommandType = type;
+                        command.CommandText = queryText;
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (queryExecCallback != null)
+                            {
+                                queryExecCallback(reader);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ SimpleWebServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWebServer
+{
+    public class Program
+    {
+        private static string baseAddress
+        {
+            get
+            {
+                return string.Format("{0}:{1}",
+                    Properties.Settings.Default.BASE_ADDRESS,
+                    Properties.Settings.Default.PORT);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+            }
+        }
+    }
+}
